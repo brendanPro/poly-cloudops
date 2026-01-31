351e06316b7262629486dbf29bbeefca1ef40650
OuidadM <mochariqouidad@gmail.com>
2026-01-31 22:33:44 +0100

    feat:automation-user-setup-n8n

---
+++ bootstrap/bootstrap.js
@@ -0,0 +1,12 @@
+import { insertUserSql } from './steps/insertUserSql.js'; 
+
+async function run() {
+  console.log('STARTING BOOTSTRAP (PURE SQL MODE)');
+  await insertUserSql();
+  console.log('\nBOOTSTRAP COMPLETE');
+}
+
+run().catch(err => {
+  console.error('\nBOOTSTRAP FAILED:', err.message);
+  process.exit(1);
+});
