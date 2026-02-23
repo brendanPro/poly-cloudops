c3ce8388eaef797b03529f95d5a4708c4386d399
Magihak <avocat0906@gmail.com>
2026-02-23 18:30:11 +0100

    fix(next.js): Deepl API key not working properly

--- polycloudops/next.config.ts
+++ polycloudops/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    deepl: process.env.DEEPL_API_KEY,
+  },
 };
 
 export default nextConfig;
